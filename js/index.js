4e597a456807fb43ea86782db0d0bd2bccc9c72a
Your Name <you@example.com>
2021-09-13 15:23:08 -0600

    modifying files

---
+++ js/index.js
@@ -0,0 +1,35 @@
+function deleteElement( event ){
+    console.log( event.target );
+    let entireElement = event.target.closest( 'li' );
+    console.log( entireElement );
+    entireElement.remove();
+
+
+
+}
+
+
+
+
+let deleteButtons= document.querySelectorAll('.shopping-item-delete');
+
+for( let i = 0; i < deleteButtons.length; i++){
+    deleteButtons[i].addEventListener("click", deleteElement);
+
+}
+
+
+function checkOrUncheck(){
+  let entireElement = element.closest('li');
+  let currentItem= entireElement.querySelector('.shoppin-item');
+  console.log(currentItem);
+
+  if ( currentItem.classList.toString().includes()('shopping-item__checked')){
+    currentItem.classList.remove('shopping-item__checked');
+
+  }else{
+
+    currentItem.classList.add('shopping-item__checked');
+  }
+
+}
